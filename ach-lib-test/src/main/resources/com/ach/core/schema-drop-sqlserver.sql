33dcfd0134bd982fd5a6483348c803179298f6d2
avinashreddy <avinashreddy.p@gmail.com>
2013-08-24 09:11:47 +0530

    1. Updated versions for spring, spring batch and logback 2. Replaced commons pool with bone cp 3. Removed unused tables for .sql files

--- ach-lib-test/src/main/resources/com/ach/core/schema-drop-sqlserver.sql
+++ ach-lib-test/src/main/resources/com/ach/core/schema-drop-sqlserver.sql
@@ -1,17 +1,7 @@
--- Drop tables created by scchem-sqlserver.xml only. Do not drop tables created by hibernate.
-drop table claimant_claim_factor;
-drop table claim_harm_amount;
-drop table claimant_position;
-drop table claimant_position_aud;
-drop table claimant_transactions;
+-- Drop tables created by schema-sqlserver.xml only. Do not drop tables created by hibernate.
 drop table claim_activity;
 drop table claimant_address;
 drop table claimant_claim;
 drop table claimant;
 drop table claimant_registration;
-drop table transaction_type;
-drop table claimant_claim_factor_aud;
-drop table claim_harm_amount_aud;
-drop table claimant_distribution_aud;
-drop table security_nav_ref;
 drop table revision_info;
